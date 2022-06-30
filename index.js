8e251ba18778c03c6baa64dbf358409e81bdd8ac
Webdev201 <107337957+webdev201@users.noreply.github.com>
2022-06-30 13:49:47 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const knex = require("./database/knex");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+// Create route for adding post data
+app.post("/posts", async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const post = await knex("posts").insert({
+      title,
+      content,
+    });
+
+    return res.status(201).json({ id: post[0], title, content });
+  } catch (err) {
+    return res.status(500).json({
+      message: " Uh oh! Something went wrong adding this post",
+    });
+  }
+});
+
+// Create route for viewing post data
+app.get("/posts", async (req, res) => {
+  const posts = await knex("posts").select("*");
+  return res.json(posts);
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port: ${PORT}`);
+});

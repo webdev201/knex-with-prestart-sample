189b24d89522700adefa774d9b4237e1199749c7
Webdev201 <107337957+webdev201@users.noreply.github.com>
2022-07-12 19:12:21 -0400

    Update create_posts.js

--- database/seeds/create_posts.js
+++ database/seeds/create_posts.js
@@ -1,13 +1,11 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex("posts").del();
+  await knex("posts").insert([
+    { id: 1, title: "Post 1", content: "Post content" },
   ]);
 };
